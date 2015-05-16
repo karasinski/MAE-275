09f88caa9b5bb9c1223b8d08a6aa2554f484e154
John Karasinski <karasinski@gmail.com>
2015-05-15 17:24:02 -0700

    add figures; update paper

--- Midterm/controller_design.m
+++ Midterm/controller_design.m
@@ -48,23 +48,74 @@ D = [0   0   0;
      0   0   0;
      0   0   0];
 
+s = tf('s');
+
+fig0 = figure;
 [a, b, c, d] = linmod('theta_dele');
 [a, b, c, d] = minreal(a, b, c, d);
 [num,den]=ss2tf(a, b, c, d);
 tf0 = tf(num,den);
 zpk(tf0)
-sisotool('bode', tf0)
+% sisotool('bode', tf0)
+c0 = -260 * ((s+0.8)*(s+0.1)) / (s*(s+2)*(s+100));
+sys0 = tf0 * c0;
+margin(sys0)
+xlim([.01 100])
+set(gcf, 'PaperPosition', [0 0 6.8493150684931505, 4.2331095119855817]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [6.8493150684931505, 4.2331095119855817]); %Set the paper to have width 5 and height 5.
+saveas(fig0, 'figures/open_comp_theta.pdf');
 
+fig0 = figure;
+bode(sys0/(1 + sys0))
+xlim([.01 100])
+set(gcf, 'PaperPosition', [0 0 6.8493150684931505, 4.2331095119855817]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [6.8493150684931505, 4.2331095119855817]); %Set the paper to have width 5 and height 5.
+pause
+saveas(fig0, 'figures/close_comp_theta.pdf');
+
+
+fig1 = figure;
 [a, b, c, d] = linmod('u_delT');
 [a, b, c, d] = minreal(a, b, c, d);
 [num,den]=ss2tf(a, b, c, d);
-tf0 = tf(num,den);
-zpk(tf0)
-sisotool('bode', tf0)
+tf1 = tf(num,den);
+zpk(tf1)
+% sisotool('bode', tf0)
+c1 = 30909 * ((s+0.1)) / (s*(s+5));
+sys1 = tf1 * c1;
+margin(sys1)
+xlim([.01 100])
+set(gcf, 'PaperPosition', [0 0 6.8493150684931505, 4.2331095119855817]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [6.8493150684931505, 4.2331095119855817]); %Set the paper to have width 5 and height 5.
+saveas(fig1, 'figures/open_comp_u.pdf');
 
+fig1 = figure;
+bode(sys1/(1 + sys1))
+xlim([.01 100])
+set(gcf, 'PaperPosition', [0 0 6.8493150684931505, 4.2331095119855817]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [6.8493150684931505, 4.2331095119855817]); %Set the paper to have width 5 and height 5.
+pause
+saveas(fig1, 'figures/close_comp_u.pdf');
+
+fig2 = figure;
 [a, b, c, d] = linmod('hdot_dele');
 [a, b, c, d] = minreal(a, b, c, d);
 [num,den]=ss2tf(a, b, c, d);
-tf0 = tf(num,den);
-zpk(tf0)
-sisotool('bode', tf0)
+tf2 = tf(num,den);
+zpk(tf2)
+% sisotool('bode', tf0)
+c2 = 0.00055 * (1) / (s);
+sys2 = tf2 * c2;
+margin(sys2)
+xlim([.01 100])
+set(gcf, 'PaperPosition', [0 0 6.8493150684931505, 4.2331095119855817]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [6.8493150684931505, 4.2331095119855817]); %Set the paper to have width 5 and height 5.
+saveas(fig2, 'figures/open_comp_hdot.pdf');
+
+fig2 = figure;
+bode(sys2/(1 + sys2))
+xlim([.01 100])
+set(gcf, 'PaperPosition', [0 0 6.8493150684931505, 4.2331095119855817]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [6.8493150684931505, 4.2331095119855817]); %Set the paper to have width 5 and height 5.
+pause
+saveas(fig1, 'figures/close_comp_hdot.pdf');
